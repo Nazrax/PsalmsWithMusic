0c660eb7cdac9ce4afeb8bc6b5df8ec7cd7ac7f9
Adrian Abraham <aabraham@us.ibm.com>
2015-05-25 21:15:37 -0400

    Add support for changing mp3 tempos

--- 2/makemp3s.sh
+++ 2/makemp3s.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 TEMPWAV=/tmp/psalm.wav
+TEMPO=$2
+[[ -z $TEMPO ]] && TEMPO=200
 
 for MIDI in ${1}-*.midi; do
  MP3=${MIDI%midi}mp3
- timidity -T200 --output-24bit -Ow -o $TEMPWAV $MIDI
+ timidity -T$TEMPO --output-24bit -Ow -o $TEMPWAV $MIDI
  lame -v $TEMPWAV $MP3
 done
